1b893058eee91d587dd63825840bcdcef303148d
Preethi-Evelyn-Sadanandan <preethievelynsadanandan@gmail.com>
2023-03-28 19:13:47 +0530

    Added Q9 of chapt 2

---
+++ Chapter2/question9.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Chapter 2 Exercises
+Q9) Write code that :
+    prints Hello if 1 stored in spam,
+    prints Howdy is 2 stored in spam, 
+    prints Greetings! if anythhing else is stored in spam
+"""
+
+spam = int(input('Enter a number.'))
+
+if spam==1:
+    print('Hello')
+elif spam==2:
+    print('Howdy')
+else:
+    print('Greetings!')
